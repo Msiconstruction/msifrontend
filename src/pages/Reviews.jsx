62deee2e5b3bc5c69543d14210cf17796b3ee744
kunalpal30 <angryp577@gmail.com>
2025-06-03 20:51:14 +0530

    Initial commit

---
+++ src/pages/Reviews.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import ClientsInteraction from '../components/ClientsInteraction';
+
+
+
+const Reviews = () => {
+  return (
+   <>
+   <ClientsInteraction/>
+   </>
+  )
+}
+
+export default Reviews
